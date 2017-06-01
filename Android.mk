omnirom/android_hardware_qcom_display-caf-msm8992

display-caf-msm8992: Don't build if TARGET_QCOM_DISPLAY_VARIANT is not caf

Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-msm8992)
 ifeq ($(call is-board-platform-in-list, thulium),true)
     TARGET_USES_SDE = true
 else
@@ -20,3 +21,4 @@ ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
 endif
+endif # TARGET_QCOM_DISPLAY_VARIANT

choose-a/android_hardware_qcom_display-caf-msm8994

Change-Id: I07934a656f50d3fb37df4b28ad76889044384d5c

Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-msm8994)
 ifeq ($(call is-board-platform-in-list, thulium),true)
     TARGET_USES_SDE = true
 else
@@ -20,3 +21,4 @@ ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
 endif
+endif # TARGET_QCOM_DISPLAY_VARIANT